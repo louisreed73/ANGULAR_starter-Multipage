03492cd1dc14da4be44605feb361cb7fcc5e3234
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2019-12-31 20:00:56 +0100

    basic multipage ANGULAR_App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes,RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GuitarsComponent } from './pages/guitars/guitars.component';
+import { BassComponent } from './pages/bass/bass.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HeadermainComponent } from './components/headermain/headermain.component';
+import { MainHomeComponent } from './components/main-home/main-home.component';
+import { MainguitarsComponent } from './components/mainguitars/mainguitars.component';
+
+const appRoutes:Routes=[
+  {path:"" ,component:HomeComponent},
+  {path:"guitars" ,component:GuitarsComponent},
+  {path:"bass" ,component:BassComponent},
+  {path:"contact" ,component:ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GuitarsComponent,
+    BassComponent,
+    ContactComponent,
+    HeadermainComponent,
+    MainHomeComponent,
+    MainguitarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+    
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+
+}
